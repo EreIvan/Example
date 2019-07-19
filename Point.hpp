6e21907e1431ed9ee0815f4c67148f745a1819c9
EreIvan <52513137+EreIvan@users.noreply.github.com>
2019-07-19 04:29:47 +0300

    Create Point.hpp

---
+++ Point.hpp
@@ -0,0 +1,24 @@
+#ifndef POINT_HPP
+#define POINT_HPP
+#include <iostream>
+
+
+class Point {
+public:
+double r_, p_;
+public:
+Point();
+Point(double ir, double ip);
+double radius() const;
+double angle() const;
+Point rotate (double alpha);
+Point operator +(Point b);
+
+ friend std::ostream& operator << (std::ostream& out, Point c);
+
+};
+
+
+     double distance(Point a, Point b);
+
+#endif POINT_HPP
